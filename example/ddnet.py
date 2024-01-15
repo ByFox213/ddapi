0f548b524bc20ee74c50b14034133ad74a716a7b
ByFox <84133700+ByFox213@users.noreply.github.com>
2024-01-15 15:49:22 +0500

    Update ddnet.py

--- example/ddnet.py
+++ example/ddnet.py
@@ -5,10 +5,10 @@ from ddapi import DDnetApi, DDPlayer
 async def main():
     obj = DDnetApi()
     nickname = "Cor"
-    user: DDPlayer = await obj.player("ByFox")
+    user: DDPlayer = await obj.player(nickname)
     if user is None:
         return "Player not found"
-    print(f"{user.hours_played_past_365_days}")
+    print(f"{user.player}: {user.hours_played_past_365_days}")
     await obj.close()  # Closing client Not necessary
     assert isinstance(user, DDPlayer)
 
