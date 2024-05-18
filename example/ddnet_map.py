9efe086421a45eee4ec97cf44d5e24cedc1d1cc6
ByFox <byfox213@gmail.com>
2024-05-18 14:57:16 +0500

    fix...

--- example/ddnet_map.py
+++ example/ddnet_map.py
@@ -4,10 +4,11 @@ from ddapi import DDnetApi, DMap
 
 async def main():
     obj = DDnetApi()
-    dd_map: DMap = await obj.map("Just Fly 13")
+    dd_map: DMap = await obj.map("Just Fly 1")
     if dd_map is None:
         await obj.close()
-        return "Map not found"
+        print("Map not found |", dd_map)
+        return
     print(f"{dd_map.name}: {dd_map.points}")
     print(dd_map.ranks[0].time)
     await obj.close()  # Closing client Not necessary
