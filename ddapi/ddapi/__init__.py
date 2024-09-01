fbd329a1f3fd2afc3c89f8ee3331c942d2a2d2ec
ByFox <84133700+ByFox213@users.noreply.github.com>
2024-09-01 17:00:01 +0500

    Update __init__.py

--- ddapi/ddapi/__init__.py
+++ ddapi/ddapi/__init__.py
@@ -1,5 +1,5 @@
 """the module is made for the convenience of users to download and use"""
-from .main import DDnetApi, DDstats, StatusAPI, utc_times, DDStatsDB
+from .main import DDnetApi, DDstats, utc_times, DDStatsDB
 from .dataclass import *
 
 __version__ = "0.5.1"
