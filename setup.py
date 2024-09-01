2fdd19d13bb34b400eda1cb88850c8bc04593483
ByFox <84133700+ByFox213@users.noreply.github.com>
2024-09-01 17:00:31 +0500

    Update setup.py
    fix __init__ file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ddapi",
-    version="0.5.1",
+    version="0.5.2",
     author="ByFox",
     description="DDnet api",
     long_description=long_description,
