6127cfb0849b4813aacf7c1e644b83e093d34055
byfox <byfox213@gmail.com>
2024-09-29 17:25:45 +0500

    tests

---
+++ tests.py
@@ -0,0 +1,59 @@
+from unittest import IsolatedAsyncioTestCase
+
+from ddapi import DDnetApi, DDstats, Player, DMap, DDPlayer, Query, Master, DDStatus
+
+
+class Tests(IsolatedAsyncioTestCase):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.test_player = ["ByFox", "Cor", "Freezestyler", "<BµmM>", "Aoe"]
+
+    async def test_dds_players(self):
+        dds = DDstats()
+        for player in self.test_player:
+            result = await dds.player(player)
+            self.assertIsInstance(result, Player)
+        self.assertNotIsInstance(
+            await dds.player("RandomNicknameRRRRRRRRR"), Player
+        )
+        await dds.close()
+
+    async def test_players(self):
+        dd = DDnetApi()
+        for player in self.test_player:
+            result = await dd.player(player)
+            self.assertIsInstance(result, DDPlayer)
+        self.assertNotIsInstance(
+            await dd.player("RandomNicknameRRRRRRRRR"), DDPlayer
+        )
+        await dd.close()
+
+    async def test_map(self):
+        dd = DDnetApi()
+        self.assertIsInstance(
+            await dd.map("Just Fly 1"), DMap
+        )
+        self.assertNotIsInstance(
+            await dd.map("Just Fly"), DMap
+        )
+        await dd.close()
+
+    async def test_master(self):
+        dd = DDnetApi()
+        result = await dd.master()
+        self.assertIsInstance(result, Master)
+        await dd.close()
+
+    async def test_query(self):
+        dd = DDnetApi()
+        result = await dd.query("ByFo")
+        self.assertIsInstance(result, Query)
+        await dd.close()
+
+    async def test_status(self):
+        dd = DDnetApi()
+        result = await dd.status()
+        self.assertIsInstance(result, DDStatus)
+        await dd.close()
+
+
