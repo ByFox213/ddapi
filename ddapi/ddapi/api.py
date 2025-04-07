b94e8a87418a626f7980344793ab9ebc7a2ee47c
ByFox <byfox213@gmail.com>
2025-04-07 19:37:34 +0500

    Fixed bugs, added autoclose, aexter, aexit and del

--- ddapi/ddapi/api.py
+++ ddapi/ddapi/api.py
@@ -1,43 +1,66 @@
+import asyncio
 from abc import ABC
-from typing import Optional, TypeVar, Type, Any
+from typing import Optional, Type, Any
 
 import aiohttp
-from aiohttp import ClientSession, ClientConnectorError
-from aiohttp.typedefs import DEFAULT_JSON_DECODER
+from aiohttp import ClientSession
 
-ModelType = TypeVar('ModelType', bound='Model')
+from ddapi.types import T
+
+try:
+    import orjson
+
+    DEFAULT_JSON_DECODER = orjson.loads
+except ImportError:
+    from aiohttp.typedefs import DEFAULT_JSON_DECODER
 
 
 class API(ABC):
     def __init__(
             self,
             session: ClientSession = None,
+            autoclose: bool = True,
             json_loads: Any = DEFAULT_JSON_DECODER
     ):
         self.__session = session
         self.json_loads = json_loads
+        self.autoclose = autoclose
 
-    @staticmethod
-    def powered() -> None:
-        return None
-
-    async def _get(self, url: str) -> Optional[dict]:
-        """Send a GET request to the given URL and return the response as JSON."""
         if self.__session is None:
             self.__session = aiohttp.ClientSession()
 
-        try:
-            async with self.__session.get(url) as req:
-                return await req.json(loads=self.json_loads) if req.status == 200 else None
-        except ClientConnectorError:
-            return
+    async def __aenter__(self):
+        if self.__session is None:
+            self.__session = aiohttp.ClientSession()
+        return self
+
+    async def __aexit__(self, _exc_type, _exc_val, _exc_tb):
+        if self.autoclose:
+            await self.close()
+
+    def __del__(self):
+        if self.__session is not None and not self.__session.closed:
+            loop = asyncio.get_event_loop()
+            if loop.is_running():
+                loop.create_task(self.close())
+            else:
+                loop.run_until_complete(self.close())
+
+    async def _get(self, url: str) -> Optional[dict]:
+        """Send a GET request to the given URL and return the response as JSON."""
+        async with self.__session.get(url) as req:
+            return (
+                await req.json(loads=self.json_loads)
+                if req.status == 200
+                else None
+            )
 
     async def _generate_model_instance(
             self,
             url: str,
-            model: Type[ModelType],
+            model: Type[T],
             k: Optional[str] = None
-    ) -> Optional[ModelType]:
+    ) -> Optional[T]:
         """Generate a model instance from the given URL and optional keyword arguments.
 
         Args:
@@ -64,3 +87,11 @@ class API(ABC):
         if self.__session is not None:
             await self.__session.close()
         self.__session = None
+
+    @property
+    def domain(self) -> str:
+        return "ddnet.org"
+
+    @staticmethod
+    def powered() -> None:
+        return None
