dd027f7fd3495a9030bb47f870e0b2c6edbaab2e
byfix <byfox213@gmail.com>
2025-05-13 18:40:41 +0500

    update version to 0.13.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="ddapi",
-    version="0.13.0",
+    version="0.13.1",
     author="ByFox",
     description="DDnet api",
     long_description=long_description,
